a51a2d8e3ac8a05a9eb6bde92c37ea78f611cb2b
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2021-11-24 13:13:11 +0100

    base rules

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "linebreak-style": ["error", "unix"],
+    "max-len": [
+      "error",
+      {
+        "code": 120,
+        "ignoreComments": true
+      }
+    ],
+    "no-console": ["warn"]
+  },
+};
