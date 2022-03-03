15964e8737b7404ef4d1bd89f2e1e2a0cdf9cfc3
Carlos Sala Samper <carlos15695@hotmail.com>
2022-03-03 09:29:52 +0100

    Restored rules to 1.0.4 version

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
-    "no-unused-vars": "off", // @typescript-eslint/no-unused-vars in types fix
-    "@typescript-eslint/no-unused-vars": "off", // @typescript-eslint/no-unused-vars in types fix
-    "@typescript-eslint/no-unused-vars-experimental": "warn", // @typescript-eslint/no-unused-vars in types fix
     quotes: ["error", "single"],
     semi: ["error", "always"],
     "linebreak-style": ["error", "unix"],
