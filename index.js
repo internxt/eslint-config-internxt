efa5991498dc26134021d4138204d566798ad9e2
Carlos Sala Samper <carlos15695@hotmail.com>
2022-03-03 09:07:04 +0100

    using no-unused-vars-experimental

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: ["plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
-    "no-unused-vars": "off", // Typescript Functions fix
+    "no-unused-vars": "off", // @typescript-eslint/no-unused-vars in types fix
+    "@typescript-eslint/no-unused-vars": "off", // @typescript-eslint/no-unused-vars in types fix
+    "@typescript-eslint/no-unused-vars-experimental": "warn", // @typescript-eslint/no-unused-vars in types fix
     quotes: ["error", "single"],
     semi: ["error", "always"],
     "linebreak-style": ["error", "unix"],
