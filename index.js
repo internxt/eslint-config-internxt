d5c4d3b4785155a3333c92d3e4bd78f971de0236
joan vicens <jwcervera@hotmail.com>
2022-09-26 10:59:07 +0200

    fix: allow escaped

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   rules: {
-    quotes: ["error", "single"],
+    quotes: ["error", "single", { avoidEscape: false }],
     semi: ["error", "always"],
     "linebreak-style": ["error", "unix"],
     "max-len": [
       "error",
       {
-        "code": 120,
-        "ignoreComments": true
-      }
+        code: 120,
+        ignoreComments: true,
+      },
     ],
-    "no-console": ["warn"]
+    "no-console": ["warn"],
   },
 };
